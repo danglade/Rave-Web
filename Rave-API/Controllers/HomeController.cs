fb65b71d7b355947f96155bac5072866adcb1ea7
Raul Danglade <rauldanglade@gmail.com>
2018-11-06 21:20:59 -0500

    Weird api stuff

---
+++ Rave-API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Rave_API.Controllers
+{
+    public class HomeController : ApiController
+    {
+        // GET: Home
+        public string Index([FromBody] JObject data)
+        {
+            return "";
+        }
+    }
+}
